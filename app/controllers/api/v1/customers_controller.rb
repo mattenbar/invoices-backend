424d1990d8abd9e952b6ff138e45f91b9a9d6a1f
mattenbar <matthw.enbar@gmail.com>
2020-08-25 15:56:19 -0400

    set up invoices controller

--- app/controllers/api/v1/customers_controller.rb
+++ app/controllers/api/v1/customers_controller.rb
@@ -1,2 +1,23 @@
 class Api::V1::CustomersController < ApplicationController
+
+  def index
+
+  end
+
+  def create
+
+  end
+
+  def show
+
+  end
+
+  def destroy
+
+  end
+
+  def customer_params
+    params.require(:customer).permit(:id, :name, :email)
+  end
+
 end
--- app/controllers/api/v1/invoices_controller.rb
+++ app/controllers/api/v1/invoices_controller.rb
@@ -1,2 +1,37 @@
 class Api::V1::InvoicesController < ApplicationController
+
+  def index
+    invoices = Invoice.all
+    render json: invoices
+
+  end
+
+  def create
+    invoice = Invoice.new(invoice_params)
+    if invoice.save
+      render json: invoices
+    else
+      render json: {error: 'Unable to creat invoice'}
+    end
+
+  end
+
+  def show
+    invoice = Invoice.find(params[:id])
+    render json: invoice
+  end
+
+  def destroy
+    invoice = Invoice.find(params[:id])
+    invoice.destroy
+  end
+
+  private
+
+  def invoice_params
+    params.require(:invoice).permit(:total, :issue_date, :due_date, :paid, :description, :amount, :price)
+  end
+
 end
+
+ 
